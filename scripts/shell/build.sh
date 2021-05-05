2879f8999139df85cff752567d097dc5cb363ded
MartinKassar <40896885+MartinKassar@users.noreply.github.com>
2021-05-05 22:38:49 +0200

    build updated

--- scripts/shell/build.sh
+++ scripts/shell/build.sh
@@ -9,7 +9,17 @@ npx google-closure-compiler \
 --js "./../../lib/rune.js" \
 --js "./../../src/scope/Manifest.js" \
 --js "./../../src/data/manifest/Resources.js" \
---js "./../../src/scene/game/Game.js" \
+--js "./../../src/Entity/Bullet.js" \
+--js "./../../src/Entity/Character.js" \
+--js "./../../src/Entity/Melons/Melon.js" \
+--js "./../../src/Entity/Melons/type/MelonL.js" \
+--js "./../../src/Entity/Melons/type/MelonM.js" \
+--js "./../../src/Entity/Melons/type/MelonS.js" \
+--js "./../../src/wave/Wave.js" \
+--js "./../../src/wave/type/Wave01.js" \
+--js "./../../src/wave/type/Wave02.js" \
+--js "./../../src/scene/Game.js" \
+--js "./../../src/scene/Menu.js" \
 --js "./../../src/system/Main.js" \
 --js "./../../src/scope/Alias.js" \
 --js_output_file "./../../dist/game.js";
