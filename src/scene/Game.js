c3c55ad0be437f903477cbd3c74ee53af88e39ac
MartinKassar <40896885+MartinKassar@users.noreply.github.com>
2021-05-08 21:30:08 +0200

    added moving clouds to the game

--- src/scene/Game.js
+++ src/scene/Game.js
@@ -18,6 +18,9 @@ game.scene.Game = function() {
     this.player = null;
     this.wave;
     this.waveCounter = 0;
+    this.m_cloudOne;
+    this.m_cloudTwo;
+    this.m_cloudThree;
 
     //--------------------------------------------------------------------------
     // Super call
@@ -46,6 +49,7 @@ game.scene.Game.prototype.constructor = game.scene.Game;
 game.scene.Game.prototype.init = function() {
     rune.scene.Scene.prototype.init.call(this);
     this.m_initBackground()
+    this.m_initClouds()
 
     this.player = new game.entity.Character();
     this.stage.addChild(this.player);
@@ -60,12 +64,14 @@ game.scene.Game.prototype.init = function() {
  */
 game.scene.Game.prototype.update = function(step) {
     rune.scene.Scene.prototype.update.call(this, step);
-    this.wave.update(step);
+    // this.wave.update(step);
     
-    if (this.wave.checkWave()){
-        this.wave = new game.wave.Wave02(this.stage);
-        this.waveCounter++;
-    }
+    // if (this.wave.checkWave()){
+    //     this.wave = new game.wave.Wave02(this.stage);
+    //     this.waveCounter++;
+    // }
+
+    this.m_cloudMotion()
 };
 
 /**
@@ -81,7 +87,56 @@ game.scene.Game.prototype.m_initBackground = function() {
         1280,
         720,
         "#FF00FF",
-        "bakgrund"
+        "cleanBkrgd"
     );
     this.stage.addChild(this.m_bkgd);
+};
+
+game.scene.Game.prototype.m_initClouds = function() {
+    this.m_cloudOne = new rune.display.Graphic(
+        20,
+        80,
+        300,
+        115,
+        "",
+        "cloud1"
+    );
+    // 20 80
+    this.stage.addChild(this.m_cloudOne);
+
+    this.m_cloudTwo = new rune.display.Graphic(
+        500,
+        90,
+        300,
+        115,
+        "",
+        "cloud2"
+    );
+    this.stage.addChild(this.m_cloudTwo);
+
+    this.m_cloudThree = new rune.display.Graphic(
+        850,
+        90,
+        300,
+        115,
+        "",
+        "cloud3"
+    );
+    this.stage.addChild(this.m_cloudThree);
+};
+
+game.scene.Game.prototype.m_cloudMotion = function() {
+    this.m_cloudOne.x +=  2
+    this.m_cloudTwo.x += 2 
+    this.m_cloudThree.x += 2 
+    console.log(this.m_cloudOne.x)
+    if(this.m_cloudOne.x >= 1270) {
+        this.m_cloudOne.x = -290
+    }
+    if(this.m_cloudTwo.x >= 1270) {
+        this.m_cloudTwo.x = -290
+    }
+    if(this.m_cloudThree.x >= 1270) {
+        this.m_cloudThree.x = -290
+    }
 };
