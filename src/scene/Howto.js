730243fbe1cc6e9f48ec772c793ce95ef3769f4a
MartinKassar <40896885+MartinKassar@users.noreply.github.com>
2021-05-20 09:53:04 +0200

    Howto-scene is now finished

--- src/scene/Howto.js
+++ src/scene/Howto.js
@@ -43,11 +43,10 @@ game.scene.Howto.prototype.init = function() {
 
     this.m_initBackground();
 
-    var keyboardimg = new rune.display.Graphic(200, 300, 251, 61, "", "keyboard");
-    this.stage.addChild(keyboardimg);
+    
+    this.m_outputText()
 
-    var backtomenu = new game.scene.Sprites();
-    this.stage.addChild(backtomenu);
+    
 };
 
 /**
@@ -56,7 +55,7 @@ game.scene.Howto.prototype.init = function() {
 game.scene.Howto.prototype.update = function(step) {
     rune.scene.Scene.prototype.update.call(this, step);
 
-    if (this.keyboard.justPressed("backspace")){
+    if (this.keyboard.justPressed("enter")){
         this.application.scenes.load([new game.scene.Menu()]);
     }
     
@@ -66,5 +65,55 @@ game.scene.Howto.prototype.m_initBackground = function() {
     this.m_bkgd = new rune.display.Graphic(0, 0, 1280, 720, "", "howtoplaybkrd");
     this.stage.addChild(this.m_bkgd);
     this.m_bkgd.alpha = 0.6;
+};
+
+game.scene.Howto.prototype.m_outputText = function() {
+    var text = new rune.text.BitmapField("Shoot all the melons and don't get hit!")
+    text.width = 600
+    text.centerX = this.application.screen.centerX;
+    text.scaleX = 3;
+    text.scaleY = 3;
+    text.y = 100;
+    this.stage.addChild(text);
+
+    var text = new rune.text.BitmapField("Controls:")
+    // text.width = 600
+    text.centerX = this.application.screen.centerX - 50;
+    text.scaleX = 3;
+    text.scaleY = 3;
+    text.y = 200;
+    this.stage.addChild(text);
+
+
+    var text = new rune.text.BitmapField("Move: A <- -> D")
+    // text.width = 600
+    text.centerX = this.application.screen.centerX -50;
+    text.scaleX = 3;
+    text.scaleY = 3;
+    text.y = 250;
+    this.stage.addChild(text);
+
+    var text = new rune.text.BitmapField("Shoot: Space")
+    // text.width = 600
+    text.centerX = this.application.screen.centerX - 50;
+    text.scaleX = 3;
+    text.scaleY = 3;
+    text.y = 300;
+    this.stage.addChild(text);
+
+
+    var text = new rune.text.BitmapField("Back to menu")
+    // text.width = 600
+    text.centerX = this.application.screen.centerX - 50;
+    text.scaleX = 4;
+    text.scaleY = 4;
+    text.y = 460;
+    this.stage.addChild(text);
+
+    var slingShot = new rune.display.Graphic(470, 460, 30, 30, "", "slangbella");
+
+    this.stage.addChild(slingShot)
+
+
 };
 
